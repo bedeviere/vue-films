f33da17cb410b0190bd1853451e61a6164f96c48
Bimata Prathama <bimata.prathama@finaccel.co>
2018-11-02 18:15:58 +0700

    added bootstrap-vue package + added movie list skeleton

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,5 +1,55 @@
 <template>
   <div class="movies">
-    <h1>This is an movies page</h1>
+    <h1>This is movies page</h1>
+    <b-pagination size="lg" :total-rows="totalResults" v-model="currentPage" :per-page="movieList.length" align="center"></b-pagination>
+    <ul>
+      <li v-for="movie in movieList" :key="movie.id">{{ movie.title }}</li>
+    </ul>
+    <p>{{ movieError.status_message }}</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+import bPagination from 'bootstrap-vue/es/components/pagination/pagination';
+
+export default {
+  name: 'Movies',
+  components: {
+    bPagination
+  },
+  data() {
+    return {
+      currentPage: 1,
+      totalResults: 0,
+      movieList: [],
+      movieError: ''
+    };
+  },
+  mounted() {
+    this.getMovies();
+  },
+  watch: {
+    currentPage: function(newVal, oldVal) {
+      this.getMovies();
+    }
+  },
+  methods: {
+    getMovies() {
+      var vm = this;
+      axios.get('https://api.themoviedb.org/3/discover/movie?api_key=e7bb37000f296041371c3e152014822e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + vm.currentPage)
+        .then(function (res) {
+          vm.movieList = res.data.results;
+          if (res.data.total_results / vm.movieList.length > 1000) {
+            vm.totalResults = vm.movieList.length * 1000;
+          } else {
+            vm.totalResults = res.data.total_results;
+          }
+        })
+        .catch(function (err) {
+          vm.movieError = err.response.data;
+        })
+    }
+  }
+}
+</script>
